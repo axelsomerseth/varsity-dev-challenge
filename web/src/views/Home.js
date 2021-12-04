f134163e15bd2a0b6f1e6b7f2a7062ba51519ff5
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2021-12-04 14:25:27 -0600

    views added

---
+++ web/src/views/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import Post from "../components/Post";
+
+const Home = () => (
+    <Fragment>
+        <Post />
+        <hr />
+    </Fragment>
+);
+
+export default Home;
