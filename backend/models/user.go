997187f1ed78243bfad4eed06dd6582517b12b0d
Axel Somerseth Córdova López <axelsomerseth@gmail.com>
2021-12-04 12:56:49 -0600

    React frontend added

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,8 +1,12 @@
 package models
 
+import "net/url"
+
 type Users struct {
 	Model
 	Username string `validate:"required,max=20"`
-	Password string `validate:"required,max=255"`
-	Email    string `validate:"required,max=255"`
+	// Password string `validate:"required,max=255"`
+	Email   string  `validate:"required,max=255"`
+	Subject string  `validate:"required"`
+	Picture url.URL `validate:"url"`
 }
